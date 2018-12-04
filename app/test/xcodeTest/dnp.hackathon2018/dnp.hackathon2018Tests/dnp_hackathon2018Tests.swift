a9283a392105ca2060d18cce52ddd6d740939fe9
Shinji Yamaguchi <mag4ndnp@gmail.com>
2018-12-04 23:03:26 +0900

    プロジェクトの起票

---
+++ app/test/xcodeTest/dnp.hackathon2018/dnp.hackathon2018Tests/dnp_hackathon2018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  dnp_hackathon2018Tests.swift
+//  dnp.hackathon2018Tests
+//
+//  Created by tofubook on 2018/12/04.
+//  Copyright © 2018年 tofubook. All rights reserved.
+//
+
+import XCTest
+@testable import dnp_hackathon2018
+
+class dnp_hackathon2018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
