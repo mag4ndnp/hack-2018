ee2027896aea0ce8a53cb259de43d884b7b9c894
Shinji Yamaguchi <mag4ndnp@gmail.com>
2018-12-09 16:06:26 +0900

    詳細画面のアニメーション切り替え

--- app/dnp.hackathon2018/dnp.hackathon2018/DetailViewController.swift
+++ app/dnp.hackathon2018/dnp.hackathon2018/DetailViewController.swift
@@ -9,20 +9,38 @@
 import UIKit
 
 class DetailViewController:UIViewController {
+    @IBOutlet weak var detailView:UIView!
+    
+    var isDetailViewToTop:Bool! = false
+    var bottomPoint:CGPoint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        bottomPoint = detailView.frame.origin
     }
     
     @IBAction func downGesture(_ sender: UISwipeGestureRecognizer) {
-        self.dismiss(animated: true, completion: nil)
-        
+        if isDetailViewToTop {
+            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                self.detailView.frame.origin.y = (self.bottomPoint?.y)!
+            }, completion: { (Bool) in
+                self.isDetailViewToTop = false
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     @IBAction func upGesture(_ sender: UISwipeGestureRecognizer) {
-        
-        
+        if !isDetailViewToTop {
+            
+            UIView.animate(withDuration: 0.3, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                self.detailView.frame.origin.y = 44
+            }, completion: { (Bool) in
+                self.isDetailViewToTop = true
+            })
+        }
     }
     
 }
